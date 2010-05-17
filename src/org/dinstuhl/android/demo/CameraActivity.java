997509ead2288bd3e2019db4aac45ed8304cc7b5
mdinstuhl <mdinstuhl@gmail.com>
2010-05-17 14:01:16 -0500

    initial commit

---
+++ src/org/dinstuhl/android/demo/CameraActivity.java
@@ -0,0 +1,15 @@
+package org.dinstuhl.android.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
